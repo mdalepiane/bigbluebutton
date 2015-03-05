a7abb9f5255570a86cbfd619ac3319983d419ae4
Mateus Dalepiane <mdalepiane@gmail.com>
2015-03-05 15:46:58 -0300

    Creating a bean for the mail service
    This commit should compile, but it DOES NOT WORK yet

---
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/mail/SendMailEvent.java
@@ -0,0 +1,7 @@
+package org.bigbluebutton.conference.service.mail;
+
+import java.util.HashMap;
+
+public class SendMailEvent {
+	protected final HashMap<String, String> eventMap = new HashMap<String, String>();
+}
