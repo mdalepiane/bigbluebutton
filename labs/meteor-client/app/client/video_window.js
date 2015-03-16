edcd60a54b994843fc3de4fa31e32eed25fe4240
Mateus Dalepiane <mdalepiane@gmail.com>
2015-03-16 18:34:24 -0300

    Simpler version of video window

--- labs/meteor-client/app/client/video_window.js
+++ labs/meteor-client/app/client/video_window.js
@@ -22,43 +22,19 @@ inviteSubmit = function() {
 
   // Send invite
   var session = ua.invite(uri, {
-    media: {
-      constraints: {
-        audio: true,
-        video: true
+      media: {
+//          stream: new webkitMediaStream(),
+          render: {
+              remote: {
+                  video: document.querySelector('video')
+              }
+          }
+      },
+      RTCConstraints: {
+          mandatory: {
+              OfferToReceiveAudio: true,
+              OfferToReceiveVideo: true
+          }
       }
-    }
   });
-
-  // Create new Session and append it to list
-  var ui = createNewSessionUI(uri, session);
-}
-
-function createNewSessionUI(uri, session) {
-  var sessionUI = {};
-
-  sessionUI.video          = document.getElementById('video-window');
-  sessionUI.renderHint     = {
-    remote: {
-      video: sessionUI.video
-    }
-  }
-
-    session.on('accepted', function (a,b) {
-      console.log("Session accepted");
-      session.mediaHandler.render(sessionUI.renderHint);
-    });
-
-    session.mediaHandler.on('addStream', function (a,b) {
-      console.log("Session addStream");
-      session.mediaHandler.render(sessionUI.renderHint);
-    });
-
-    session.on('rejected', function (a,b) {
-        console.warn("Session rejected");
-    });
-
-    session.on('failed', function (a,b) {
-        console.warn("Session failed");
-    });
 }
