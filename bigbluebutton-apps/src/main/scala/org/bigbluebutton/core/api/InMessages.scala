afabe5c0945ed357b805828ac533de469abdd6dd
Mateus Dalepiane <mdalepiane@gmail.com>
2015-04-08 17:21:12 -0300

    Store active talker and sip video status in bbb-apps
    The active talker and sip video status (present or not) are now stored
    in bbb-apps. Clients that join the conference might request this data
    to decide how to handle sip video stream.
    
    Conflicts:
    	bigbluebutton-apps/src/main/webapp/WEB-INF/red5-web.xml

--- bigbluebutton-apps/src/main/scala/org/bigbluebutton/core/api/InMessages.scala
+++ bigbluebutton-apps/src/main/scala/org/bigbluebutton/core/api/InMessages.scala
@@ -481,6 +481,11 @@ case class SipVideoResumed (
     meetingID: String
 ) extends InMessage
 
+case class ActiveTalkerChanged (
+    meetingID: String,
+    activeTalker: String
+) extends InMessage
+
 // Whiteboard
 case class SendWhiteboardAnnotationRequest(
     meetingID: String, 
